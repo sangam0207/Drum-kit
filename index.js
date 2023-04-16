da5a382262d968bb8f0e977a69ed76454f7acd24
sangam <ss7618796@gmail.com>
2023-04-16 20:10:32 +0530

    drum-kit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+//detecting keypress;
+for (var i = 0; i < 7; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+         animation(buttonInnerHtml);
+    });
+}
+//detecting keypress;
+document.addEventListener("keypress",function(e){
+    makeSound(e.key);
+    animation(e.key);
+})
+function makeSound(key){
+        switch (key) {
+            case "w":
+                var audio = new Audio("./sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("./sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("./sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("./sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("./sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("./sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("./sounds/crash.mp3");
+                audio.play();
+                break;
+            default:
+                break;
+        }
+    }
+    function animation(key){
+        var activebtn=document.querySelector("."+key+"_drum");
+        activebtn.classList.add("prassed");
+        setTimeout(function(){
+            activebtn.classList.remove("prassed");
+        },100);
+    }
+
